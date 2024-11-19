d1938d650a1c3eddad5bc1a84463f1f1e7d7131d
donatimar <donatimar@gmail.com>
2024-11-19 14:49:27 +0100

    added static assets

--- app.js
+++ app.js
@@ -2,35 +2,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 const posts = [
   {
     titolo: "Il mio primo post",
     contenuto: "Questo è il contenuto del primo post",
-    immagine: "",
+    immagine: "/images/ciambellone.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "Il mio secondo post",
     contenuto: "Questo è il contenuto del secondo post",
-    immagine: "",
+    immagine: "/images/cracker_barbabietola.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "Il mio terzo post",
     contenuto: "Questo è il contenuto del terzo post",
-    immagine: "",
+    immagine: "/images/pane_fritto_dolce.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "Il mio quarto post",
     contenuto: "Questo è il contenuto del quarto post",
-    immagine: "",
+    immagine: "/images/pasta_barbabietola.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
   {
     titolo: "Il mio quinto post",
     contenuto: "Questo è il contenuto del quinto post",
-    immagine: "",
+    immagine: "/images/torta_paesana.jpeg",
     tags: ["tag1", "tag2", "tag3"],
   },
 ];
