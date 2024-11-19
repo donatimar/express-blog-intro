0f0cce073c8e524b718456f665953fc79d033b02
donatimar <donatimar@gmail.com>
2024-11-19 14:22:36 +0100

    created array

--- app.js
+++ app.js
@@ -2,6 +2,39 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const posts = [
+  {
+    titolo: "Il mio primo post",
+    contenuto: "Questo è il contenuto del primo post",
+    immagine: "",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "Il mio secondo post",
+    contenuto: "Questo è il contenuto del secondo post",
+    immagine: "",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "Il mio terzo post",
+    contenuto: "Questo è il contenuto del terzo post",
+    immagine: "",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "Il mio quarto post",
+    contenuto: "Questo è il contenuto del quarto post",
+    immagine: "",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    titolo: "Il mio quinto post",
+    contenuto: "Questo è il contenuto del quinto post",
+    immagine: "",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+];
+
 app.get("/", (req, res) => {
   const message = { message: "Server del mio blog" };
   res.json(message);
